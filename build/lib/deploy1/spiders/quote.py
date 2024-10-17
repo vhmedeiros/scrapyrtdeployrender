0a1a06cd5170faec772e14e0d350679cee35e548
Victor Medeiros <victorhugomls@hotmail.com>
2024-10-17 09:22:57 -0300

    initial

---
+++ build/lib/deploy1/spiders/quote.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class QuoteSpider(scrapy.Spider):
+    name = "quote"
+    start_urls = ["https://quotes.toscrape.com"]
+
+    def start_requests(self):
+        yield scrapy.Request(f'https://quotes.toscrape.com/tag/{self.tag}')
+
+    def parse(self, response, **kwargs):
+        quotes = response.xpath('//div[@class="quote"]')
+        for quote in quotes:
+            yield {
+                'quote': quote.xpath('.//span//text()').get(),
+                'author': quote.xpath('.//small[@class="author"]//text()').get(),
+                'tag': quote.xpath('.//a[@class="tag"]//text()').getall()
+            }
